a229d2185df3867cd3153633144ceacaaab5f33c
Leonte Cosmin <62109211+LCCosmin@users.noreply.github.com>
2020-04-13 10:04:11 +0300

    initial commit

---
+++ db/db.php
@@ -0,0 +1,8 @@
+<?php
+$con=mysqli_connect("46.214.70.34","2019_leonte","cnamcnam","2019_leonte");
+		// Check connection
+		if (mysqli_connect_errno())
+		  {
+		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		  }
+?>
